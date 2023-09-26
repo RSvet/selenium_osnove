9784f9ece68771d0839783151bce53ce69a4c2ab
Svetlana Radisic <svetlana_radisic@yahoo.com>
2023-09-26 22:11:25 +0200

    Add Zadatak4 with Select class

---
+++ src/main/java/p26_09_2023/Zadatak4.java
@@ -0,0 +1,18 @@
+package p26_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Zadatak4 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+        Select category = new Select(driver.findElement(By.id("gh-cat")));
+        category.selectByValue("14339");
+
+    }
+}
