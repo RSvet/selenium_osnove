7d0d65f25e473d458d36c5fdbea8d042e7b0782a
Svetlana Radisic <svetlana_radisic@yahoo.com>
2023-10-10 14:31:41 +0200

    Add LeftNavPage for elements on left navigation on login page for swag labs website

---
+++ src/main/java/pages/LeftNavPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class LeftNavPage extends BasicPage {
+    public LeftNavPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void waitLeftNavMenu(){
+        wait
+                .withMessage("Left menu did not appear")
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("bm-item-list")));
+    }
+    public boolean doesLogoutButtonExist() {
+        return elementExists(By.linkText("Logout"), 0);
+    }
+
+    public WebElement getLogoutLink() {
+        return driver.findElement(By.linkText("Logout"));
+    }
+
+    public void clickLogoutButton(){
+        getLogoutLink().click();
+    }
+
+
+
+
+}
